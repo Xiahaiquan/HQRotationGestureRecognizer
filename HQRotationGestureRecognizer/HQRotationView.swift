7ae50133f4a7b02a0f1f83412c1e2fee54349516
HaiQuan <710565793@qq.com>
2017-04-12 13:14:53 +0800

    Revert "Revert "第一次提交""
    This reverts commit 5cbe71e309804c37fa5aa6644e481341e173a88b.

---
+++ HQRotationGestureRecognizer/HQRotationView.swift
@@ -0,0 +1,223 @@
+//
+//  RotationView.swift
+//  ango
+//
+//  Created by HaiQuan on 2017/3/19.
+//  Copyright © 2017年 HaiQuan. All rights reserved.
+//
+
+import UIKit
+
+protocol MyDelegate {
+    func didAcceptSomethiong(_ someoneName : Int)
+}
+
+class HQRotationView: UIView {
+    
+    
+    var delegate : MyDelegate?
+    
+//    typealias jumpCloseBage = (_ num: Int) -> ()
+//    
+//    var jump = jumpCloseBage?()
+    
+   
+    
+//    typealias selectCloseBage = (_ selet: Int) -> Void
+//    var selected = selectCloseBage?()
+    //Cannot invoke initalizer for type "Bage" with no arguments
+    var  _btnArray = [UIButton]()
+    
+    var  rotationAngleInRadians: CGFloat = 0
+    var  radians: CGFloat = 0
+    
+    
+    
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.layer.cornerRadius = self.frame.size.width / 2 ;
+        self.width = self.frame.size.width;
+        
+        
+        self.addGestureRecognizer(HQUIGestureRecognizer.init(target: self, action: #selector(changeMove)))
+        
+        
+        self.isUserInteractionEnabled = true
+        
+        //        self.backgroundColor = .yellow
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func Btn(BtnWidth: CGFloat, sizeWith: CGFloat, mask: Bool, radius:  CGFloat, image: [String],titileArray: [String]) {
+        let corner = (-M_PI * 2.0 / Double(titileArray.count ))
+        // 半径为 （转盘半径➖按钮半径）的一半
+        let  r = (self.width  - BtnWidth) / 2 ;
+        let  x = self.width  / 2 ;
+        let  y = self.width  / 2 ;
+        
+        
+        for i in 0 ..< titileArray.count {
+            
+            let  btn = UIButton.init(type: .custom)
+            btn.frame = CGRect.init(x: 0, y: 0, width: BtnWidth, height: BtnWidth)
+            btn.layer.masksToBounds = mask;
+            btn.layer.cornerRadius = radius;
+            
+            let x1 = x + r * CGFloat(cos(corner * Double(i)))
+            let y1 = y + r * CGFloat(sin(corner * Double(i)))
+            btn.center =  CGPoint.init(x: x1, y: y1)
+            
+            
+            
+            btn.tag = i;
+            
+            btn.setTitle(titileArray[i], for: .normal)
+            btn.titleLabel?.font = UIFont.systemFont(ofSize: 10)
+            btn.titleLabel?.backgroundColor = .blue
+            btn.setImage(UIImage.init(named: image[i]), for: .normal)
+            btn.titleEdgeInsets = UIEdgeInsetsMake(0, -btn.imageView!.intrinsicContentSize.width , -(btn.imageView!.intrinsicContentSize.height+2.0/2.0), 0)
+            
+            btn.imageEdgeInsets = UIEdgeInsetsMake(-(btn.titleLabel!.intrinsicContentSize.height+2.0/2.0), 0, 0, -btn.titleLabel!.intrinsicContentSize.width)
+            
+            //                    if (i == 0) {
+            //                            btn.backgroundColor = UIColor.yellow
+            //                    }else if (i == 1) {
+            //                        btn.backgroundColor = UIColor.blue
+            //
+            //                    }else if (i == 2) {
+            //                        btn.backgroundColor = UIColor.gray
+            //
+            //                    }else {
+            //                        btn.backgroundColor = UIColor.red
+            //            }
+            
+            
+            btn.addTarget(self, action: #selector(btnClick), for: .touchUpInside)
+            self.addSubview(btn)
+            _btnArray.append(btn)
+            
+            
+            
+        }
+    }
+    
+    
+    func btnClick(_ sender: UIButton) {
+        
+        if jump != nil {
+            
+            jump!(sender.tag)
+        }
+    }
+    
+    func selectBtn() {
+        
+        let f = self.rotationAngleInRadians*CGFloat(180/M_PI);
+        
+        
+        let  i = fabs(f) / 90;
+        
+        
+        let s = i.truncatingRemainder(dividingBy: 4)
+        
+        
+        delegate?.didAcceptSomethiong(Int(s))
+        
+        
+    }
+    
+}
+
+extension HQRotationView {
+    
+    func changeMove(_ retation:HQUIGestureRecognizer ) {
+        
+        
+        switch (retation.state) {
+            
+            
+            
+        case .changed:
+            
+            self.rotationAngleInRadians += retation.rotation
+            
+            let r = self.rotationAngleInRadians + retation.rotation
+            
+            UIView.animate(withDuration: 0.25, animations: {
+                
+                self.transform = CGAffineTransform(rotationAngle: r)
+                
+                for btn in self._btnArray {
+                    
+                    btn.transform = CGAffineTransform(rotationAngle: -r)
+                }
+            })
+            
+            
+            radians = r
+            
+            
+            
+        case .ended:
+            
+            let  num = Int(self.rotationAngleInRadians / CGFloat(M_PI/2))
+            let  last = Int(self.rotationAngleInRadians * CGFloat(180/M_PI)) % 60
+            
+            //            int last = ((int)(self.rotationAngleInRadians*(180/M_PI)))%(60);
+            
+            
+            //            print(last)
+            if (abs(last)>=30) {
+                
+                var r: Double = 0
+                UIView.animate(withDuration: 0.25, animations: {
+                    
+                    r = (M_PI / 2) * (Double(num) + Double(1))
+                    
+                    self.transform = CGAffineTransform(rotationAngle: CGFloat(r))
+                    
+                    for btn in self._btnArray {
+                        btn.transform = CGAffineTransform(rotationAngle: CGFloat(-r))
+                    }
+                })
+                radians = CGFloat(r)
+                self.rotationAngleInRadians = CGFloat(r)
+                
+                
+            }
+            else{
+                var n: Double = 0
+                UIView.animate(withDuration: 0.25, animations: {
+                    
+                    n = (M_PI / 2) * Double(num)
+                    
+                    self.transform = CGAffineTransform(rotationAngle: CGFloat(n))
+                    
+                    
+                    for btn in self._btnArray {
+                        btn.transform = CGAffineTransform(rotationAngle: CGFloat(-n));
+                    }
+                })
+                
+                radians = CGFloat(n)
+                self.rotationAngleInRadians = CGFloat(n)
+                
+                
+            }
+            
+            self.selectBtn()
+            
+        default:
+            break
+        }
+        
+        
+    }
+    
+}
---
+++ HQRotationGestureRecognizer/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  HQRotationGestureRecognizer
+//
+//  Created by HaiQuan on 2017/4/11.
+//  Copyright © 2017年 HaiQuan. All rights reserved.
+//
+
+import UIKit
+
+
+let WIDTH_SCREEN = UIScreen.main.bounds.width
+
+
+let HEIGHT_SCREEN = UIScreen.main.bounds.height
+
+let rotBtnsWidth: CGFloat =  80
+
+class ViewController: UIViewController {
+
+    fileprivate var romate: HQRotationView = {
+        
+        let rotatePlateWidth = WIDTH_SCREEN / ( 1.4)
+        let romate = HQRotationView(frame:CGRect(x: 0, y: 0, width: rotatePlateWidth, height: rotatePlateWidth))
+        
+        let imageArray = ["ango_icon_xuanzhong_adas setting_100pc","ango_icon_xuanzhong_me_100px","ango_icon_xuanzhong_money_100px","ango_icon_xuanzhong_setting_icon_100px"]
+        let titleArray = ["HomeMe","HomeSet","ADAS","HomeFortune"]
+        romate.Btn(BtnWidth: rotBtnsWidth, sizeWith: rotBtnsWidth, mask: true, radius: rotBtnsWidth/2, image: imageArray, titileArray: titleArray)
+        
+        
+        return romate
+        
+        
+        
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addRomateView()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func addRomateView() {
+        view.addSubview(romate)
+        romate.centerY = view.centerY
+        romate.centerX = view.centerX.multiplied(by: 1.5)
+        
+        
+        romate.delegate = self
+        
+        
+        romate.jump = { (num) in
+            
+            self.selectedCategoryEndWithIndex(num)
+        }
+        
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+extension ViewController: MyDelegate {
+    func didAcceptSomethiong(_ someoneName: Int) {
+        print(someoneName)
+    }
+}
+
